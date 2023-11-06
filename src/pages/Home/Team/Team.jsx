789ed6b265f940443b3b8205c4b95e13075159dc
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-06 06:12:55 +0600

    team update

--- src/pages/Home/Team/Team.jsx
+++ src/pages/Home/Team/Team.jsx
@@ -14,6 +14,7 @@ const Team = () => {
   return (
     <div className="max-w-7xl mx-auto my-32 p-5 md:p-4 lg:p-2">
       <h2 className="text-4xl font-bold text-center">Meet The Team</h2>
+      <p className="max-w-4xl mx-auto text-center text-lg mt-5 mb-24 p-5 md:p-4 lg:p-2">At Share Eats, we believe that every great initiative is powered by the people behind it. Our team is a diverse group of passionate individuals who share a common goal - to reduce food waste and ensure that no one in our community goes hungry. Get to know the faces and stories that drive our mission forward.</p>
       <div className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4  my-20 p-5 md:p-4 lg:p-2">
         {teams.map((person) => (
           <Person key={person.id} person={person}></Person>
