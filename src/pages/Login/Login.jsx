f6c9e86abfa342e7bacf8eda034dcfac0a2f863d
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-07 12:05:59 +0600

    login register design update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,11 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import { BsPersonCircle } from "react-icons/bs";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./Login.css";
 
 const Login = () => {
   const { signIn, signInWithGoogle } = useContext(AuthContext);
@@ -57,37 +59,38 @@ const Login = () => {
   };
 
   return (
-    <div className="hero bg-base-200 pt-12 pb-32">
+    <div className="bg_image hero bg-base-200 pt-6 pb-12">
       <ToastContainer position="top-center" />
       <div className="hero-content flex-col">
-        <div className="text-center lg:text-left">
-          <h1 className="text-3xl md:text-5xl font-bold mb-2">
-            Please Login Here
-          </h1>
+        <div className="flex flex-col justify-center items-center text-center lg:text-left text-white">
+          <h1 className="text-2xl md:text-4xl font-bold mb-2">Welcome</h1>
+          <span className=" mb-5 text-lg">to Your account</span>
+          <BsPersonCircle className="text-[80px]" />
         </div>
-        <div className=" w-full  rounded-xl hover:shadow-2xl hover:shadow-[#429FFD] shadow-2xl bg-base-100 mt-10 pb-8">
+        <div className=" w-full md:min-w-[450px]  rounded-xl bg-base-100 mt-4 pb-8">
           <form onSubmit={handleSubmit} className="card-body">
             <div className="form-control">
-              <label className="label font-semibold">
-                <span className="label-text">Email</span>
-              </label>
+              <div className="text-sm font-bold text-gray-700 tracking-wide">
+                Email
+              </div>
               <input
                 type="email"
                 name="email"
-                placeholder="Email"
-                className="ring-1 ring-gray-300 bg-transparent px-4 py-3 mt-2 rounded-full outline-none focus:ring-2 focus:ring-[#429FFD]"
+                placeholder="Type your email"
+                className="w-full text-lg py-2 border-b-2 border-[#D70F64] focus:outline-none"
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label font-semibold">
-                <span className="label-text">Password</span>
-              </label>
+
+            <div className="form-control mt-5">
+              <div className="text-sm font-bold text-gray-700 tracking-wide">
+                Password
+              </div>
               <input
                 type="password"
                 name="password"
-                placeholder="Password"
-                className="ring-1 ring-gray-300 bg-transparent px-4 py-3 mt-2 rounded-full outline-none focus:ring-2 focus:ring-[#429FFD]"
+                placeholder="Type your password"
+                className="w-full text-lg py-2 border-b-2 border-[#D70F64] focus:outline-none"
                 required
               />
               <label className="label font-semibold">
@@ -100,22 +103,26 @@ const Login = () => {
             {passwordError && <p className="text-red-500">{passwordError}</p>}
 
             <div className="form-control mt-6">
-              <button className="btn rounded-full  text-white bg-[#429FFD] hover:bg-transparent hover:border-2 hover:border-[#429FFD] hover:text-[#429FFD]">
+              <button className="btn rounded-lg  text-white bg-[#D70F64] hover:bg-transparent hover:border-2 hover:border-[#D70F64] hover:text-[#D70F64]">
                 Login
               </button>
             </div>
-            <p className="text-center font-semibold">Or</p>
+            <p className="flex items-center justify-center space-x-2">
+              <span className="h-px bg-gray-400 w-14"></span>
+              <span className="font-normal text-[#D70F64]">Or Login with</span>
+              <span className="h-px bg-gray-400 w-14"></span>
+            </p>
             <button
               onClick={handleSignInWithGoogle}
-              className="btn btn-outline rounded-full border-2 border-[#429FFD]  hover:bg-[#429FFD] hover:text-white hover:border-none "
+              className="btn btn-outline rounded-lg border-2 border-[#D70F64]  hover:bg-[#D70F64] hover:text-white hover:border-none "
             >
               <FcGoogle className="text-2xl" />{" "}
               <span className="ml-2">Google</span>{" "}
             </button>
           </form>
           <p className="text-center">
-            New in Dream Wedding?
-            <Link to="/register" className="text-[#429FFD] ml-2 font-bold">
+            New in Share Eats?
+            <Link to="/register" className="text-[#D70F64] ml-2 font-bold">
               Register
             </Link>
           </p>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -79,60 +79,62 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar max-w-7xl mx-auto">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="py-2 shadow-xl">
+      <div className="navbar max-w-7xl mx-auto ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {links}
-          </ul>
+              {links}
+            </ul>
+          </div>
+          <img src={logo} alt="" className="max-w-[60px] cursor-pointer mr-2" />
+          <p className="text-lg lg:text-2xl font-semibold cursor-pointer">
+            Share Eats
+          </p>
         </div>
-        <img src={logo} alt="" className="max-w-[60px] cursor-pointer mr-2" />
-        <p className="text-lg lg:text-2xl font-semibold cursor-pointer">
-          Share Eats
-        </p>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-lg">{links}</ul>
-      </div>
-      <div className="navbar-end">
-        {user?.uid ? (
-          <>
-            <NavLink
-              className="btn border-2 border-[#D70F64] bg-transparent text-[#D70F64] hover:bg-[#D70F64] hover:text-white px-8"
-              onClick={handleLogOut}
-            >
-              Logout
-            </NavLink>
-          </>
-        ) : (
-          <>
-            <NavLink
-              to="/login"
-              className="btn font-semibold bg-[#D70F64] text-white hover:border-2 hover:border-[#D70F64] hover:bg-transparent hover:text-[#D70F64] px-8 py-2"
-            >
-              Login
-            </NavLink>
-          </>
-        )}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-lg">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          {user?.uid ? (
+            <>
+              <NavLink
+                className="btn border-2 border-[#D70F64] bg-transparent text-[#D70F64] hover:bg-[#D70F64] hover:text-white px-8"
+                onClick={handleLogOut}
+              >
+                Logout
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink
+                to="/login"
+                className="btn font-semibold bg-[#D70F64] text-white hover:border-2 hover:border-[#D70F64] hover:bg-transparent hover:text-[#D70F64] px-8 py-2"
+              >
+                Login
+              </NavLink>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
