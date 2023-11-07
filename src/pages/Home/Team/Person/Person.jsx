f43531eff22c1649cb6c8afd6022b2a384bde397
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-07 08:54:27 +0600

    team color update

--- src/pages/Home/Team/Person/Person.jsx
+++ src/pages/Home/Team/Person/Person.jsx
@@ -4,8 +4,8 @@ const Person = ({ person }) => {
   const { name, photo, designation } = person;
 
   return (
-    <div>
-      <img src={photo} className="border-4 border-black rounded-full" alt="" />
+    <div className="border-2 border-base-200 hover:border-[#D70F64] rounded-xl p-5">
+      <img src={photo} className=" rounded-full" alt="" />
       <div className="mt-5">
         <h2 className="text-2xl font-bold text-center">{name}</h2>
         <p className="font-semibold text-center">{designation}</p>
