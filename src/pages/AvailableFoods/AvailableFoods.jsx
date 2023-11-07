5da363d01b9790e00de753f0279701ec7aaf68a3
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-07 07:48:43 +0600

    React Helmet used for dynamic title

--- src/pages/AvailableFoods/AvailableFoods.jsx
+++ src/pages/AvailableFoods/AvailableFoods.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import FoodCard from "../Home/FeaturedFoods/FoodCard/FoodCard";
 
 const AvailableFoods = () => {
@@ -12,6 +13,9 @@ const AvailableFoods = () => {
 
   return (
     <div className="max-w-7xl mx-auto my-12 p-5 md:p-4 lg:p-2">
+      <Helmet>
+        <title>Share Eats | AvailableFoods</title>
+      </Helmet>
       <h2 className="text-4xl font-bold text-center">Available Foods</h2>
       <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-20 ">
         {availableFoods.map((food) => (
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,17 +1,21 @@
+import { Helmet } from "react-helmet";
 import Banner from "../Banner/Banner";
 import FAQ from "../FAQ/FAQ";
 import FeaturedFoods from "../FeaturedFoods/FeaturedFoods";
 import Team from "../Team/Team";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <FeaturedFoods></FeaturedFoods>
-            <Team></Team>
-            <FAQ></FAQ>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Share Eats | Home</title>
+      </Helmet>
+      <Banner></Banner>
+      <FeaturedFoods></FeaturedFoods>
+      <Team></Team>
+      <FAQ></FAQ>
+    </div>
+  );
 };
 
 export default Home;
