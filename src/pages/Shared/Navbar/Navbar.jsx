43bafad520862a4611773f3e4407fbd3f175e7a3
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-07 09:40:15 +0600

    Navbar update

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -25,8 +25,7 @@ const Navbar = () => {
         <NavLink
           to="/"
           className={`${
-            activeRoute === "/" &&
-            "!text-[#429FFD] font-bold underline !bg-inherit"
+            activeRoute === "/" && "!text-[#D70F64] font-bold  !bg-inherit"
           }`}
         >
           Home
@@ -37,7 +36,7 @@ const Navbar = () => {
           to="/availablefoods"
           className={`${
             activeRoute === "/availablefoods" &&
-            "!text-[#429FFD] font-bold underline !bg-inherit"
+            "!text-[#D70F64] font-bold  !bg-inherit"
           }`}
         >
           Available Foods
@@ -48,7 +47,7 @@ const Navbar = () => {
           to="/addfood"
           className={`${
             activeRoute === "/addfood" &&
-            "!text-[#429FFD] font-bold underline !bg-inherit"
+            "!text-[#D70F64] font-bold  !bg-inherit"
           }`}
         >
           Add Food
@@ -59,7 +58,7 @@ const Navbar = () => {
           to="/managemyfoods"
           className={`${
             activeRoute === "/managemyfoods" &&
-            "!text-[#429FFD] font-bold underline !bg-inherit"
+            "!text-[#D70F64] font-bold  !bg-inherit"
           }`}
         >
           Manage My Foods
@@ -70,78 +69,70 @@ const Navbar = () => {
           to="/myfoodrequest"
           className={`${
             activeRoute === "/myfoodrequest" &&
-            "!text-[#429FFD] font-bold underline !bg-inherit"
+            "!text-[#D70F64] font-bold  !bg-inherit"
           }`}
         >
           My Food Request
         </NavLink>
       </li>
-
-      {user?.uid ? (
-        <>
-          <li>
-            <NavLink
-              className="my-2 lg:my-0 !border-[#429FFD] !border-2 !bg-transparent !text-[#429FFD]  font-semibold "
-              onClick={handleLogOut}
-            >
-              Logout
-            </NavLink>
-          </li>
-        </>
-      ) : (
-        <>
-          <li>
-            <NavLink
-              to="/login"
-              className={`${
-                activeRoute === "/login" &&
-                "!text-[#429FFD] font-bold underline !bg-inherit"
-              }`}
-            >
-              Login
-            </NavLink>
-          </li>
-        </>
-      )}
     </>
   );
 
   return (
-    <div className=" top-0 z-50 shadow-xl">
-      <div className="navbar max-w-7xl mx-auto">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+    <div className="navbar max-w-7xl mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {links}
-            </ul>
-          </div>
-          <img src={logo} alt="" className="max-w-[60px] cursor-pointer mr-2" />
-          <p className="text-lg lg:text-2xl font-semibold cursor-pointer">
-            Share Eats
-          </p>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links}
+          </ul>
         </div>
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{links}</ul>
-        </div>
+        <img src={logo} alt="" className="max-w-[60px] cursor-pointer mr-2" />
+        <p className="text-lg lg:text-2xl font-semibold cursor-pointer">
+          Share Eats
+        </p>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-lg">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        {user?.uid ? (
+          <>
+            <NavLink
+              className="btn border-2 border-[#D70F64] bg-transparent text-[#D70F64] hover:bg-[#D70F64] hover:text-white px-8"
+              onClick={handleLogOut}
+            >
+              Logout
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink
+              to="/login"
+              className="btn font-semibold bg-[#D70F64] text-white hover:border-2 hover:border-[#D70F64] hover:bg-transparent hover:text-[#D70F64] px-8 py-2"
+            >
+              Login
+            </NavLink>
+          </>
+        )}
       </div>
     </div>
   );
