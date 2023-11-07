fa2a4000e4c1f26397c663f1de317b4b1ef51ec1
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-07 21:53:33 +0600

    food added data post to database

--- src/pages/AddFood/AddFood.jsx
+++ src/pages/AddFood/AddFood.jsx
@@ -1,6 +1,76 @@
 import { Helmet } from "react-helmet";
 import HeroPages from "../../components/HeroPages/HeroPages";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
 const AddFood = () => {
+  const { user } = useContext(AuthContext);
+
+  const [userDb, setUserDb] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setUserDb(data));
+  }, [user?.email]);
+
+  const handleAddFood = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const foodName = form.food_name.value;
+    const foodImage = form.food_image.value;
+    const foodQuantity = form.food_quantity.value;
+    const pickupLocation = form.pickup_location.value;
+    const expiredDate = form.expired_date.value;
+    const foodStatus = form.food_status.value;
+    const additionalNotes = form.additional_notes.value;
+    const donatorImage = userDb.photo;
+    const donatorName = userDb.name;
+    const donatorEmail = userDb.email;
+
+    const food = {
+      foodImage,
+      foodName,
+      donatorImage,
+      donatorName,
+      donatorEmail,
+      foodQuantity,
+      pickupLocation,
+      foodStatus,
+      expiredDate,
+      additionalNotes,
+    };
+
+    console.log(food);
+
+    form.reset();
+
+    fetch("http://localhost:5000/foods", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(food),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Successfully Added Product",
+            icon: "success",
+            confirmButtonText: "OK",
+          });
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div>
       <HeroPages name="Add Food"></HeroPages>
@@ -10,7 +80,10 @@ const AddFood = () => {
           <title>Share Eats | Add Food</title>
         </Helmet>
 
-        <form className="shadow-lg shadow-[#D70F64] rounded-xl mb-24 lg:px-2 px-5 pt-6 pb-8 hover:shadow-2xl hover:shadow-[#D70F64] mt-10">
+        <form
+          onSubmit={handleAddFood}
+          className="shadow-lg shadow-[#D70F64] rounded-xl mb-24 lg:px-2 px-5 pt-6 pb-8 hover:shadow-2xl hover:shadow-[#D70F64] mt-10"
+        >
           {/* Food Info  */}
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
             <label
@@ -89,7 +162,6 @@ const AddFood = () => {
               required
             />
           </div>
-
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
             <label
               className="block text-[#D70F64] font-bold mb-2"
@@ -105,7 +177,6 @@ const AddFood = () => {
               required
             />
           </div>
-
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
             <label
               className="block text-[#D70F64] font-bold mb-2"
@@ -134,7 +205,7 @@ const AddFood = () => {
               className="ring-1 rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight  focus:ring-2 focus:ring-[#D70F64]"
               name="donator_email"
               type="text"
-              defaultValue="xyz@gmail.com"
+              defaultValue={userDb.email}
               readOnly
             />
           </div>
@@ -149,7 +220,7 @@ const AddFood = () => {
               className="ring-1 rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight  focus:ring-2 focus:ring-[#D70F64]"
               name="donator_name"
               type="text"
-              defaultValue="xyz"
+              defaultValue={userDb.name}
               readOnly
             />
           </div>
@@ -164,11 +235,10 @@ const AddFood = () => {
               className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
               name="donator_image"
               type="text"
-              defaultValue="abc"
+              defaultValue={userDb.photo}
               readOnly
             />
           </div>
-
           <div className="flex items-center justify-center">
             <input
               type="submit"
