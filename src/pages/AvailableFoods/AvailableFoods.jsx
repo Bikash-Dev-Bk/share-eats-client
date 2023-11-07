76b5ed78d3934505a00a9433adf0086e4ba00633
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-07 06:49:42 +0600

    Available food added

--- src/pages/AvailableFoods/AvailableFoods.jsx
+++ src/pages/AvailableFoods/AvailableFoods.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import FoodCard from "../Home/FeaturedFoods/FoodCard/FoodCard";
+
 const AvailableFoods = () => {
-    return (
-        <div>
-            <h2>This is AvailableFoods</h2>
-        </div>
-    );
+  const [availableFoods, setAvailableFoods] = useState([]);
+
+  useEffect(() => {
+    fetch("f.json")
+      .then((res) => res.json())
+      .then((data) => setAvailableFoods(data));
+  }, []);
+
+  return (
+    <div className="max-w-7xl mx-auto my-12 p-5 md:p-4 lg:p-2">
+      <h2 className="text-4xl font-bold text-center">Available Foods</h2>
+      <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-20 ">
+        {availableFoods.map((food) => (
+          <FoodCard key={food.id} food={food}></FoodCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AvailableFoods;
