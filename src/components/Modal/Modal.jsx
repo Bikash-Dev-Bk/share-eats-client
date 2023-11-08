d421280d746a011ec2cf94d3a868df16cbb7f265
bikash-dev <bikash.dev.bk@gmail.com>
2023-11-08 12:20:21 +0600

    food request done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,41 @@
-import { Helmet } from "react-helmet";
-import HeroPages from "../../components/HeroPages/HeroPages";
+import Swal from "sweetalert2";
+import PropTypes from "prop-types";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
-import { useEffect, useState } from "react";
-import Swal from "sweetalert2";
 
-const AddFood = () => {
+const Modal = ({ food }) => {
+  const {
+    _id,
+    foodImage,
+    foodName,
+    donatorImage,
+    donatorName,
+    donatorEmail,
+    pickupLocation,
+    expiredDate,
+    additionalNotes,
+  } = food;
+
   const { user } = useContext(AuthContext);
 
-  const [userDb, setUserDb] = useState([]);
+  const date = new Date();
+  const formattedDate = date.toLocaleString("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/users/${user?.email}`)
-      .then((res) => res.json())
-      .then((data) => setUserDb(data));
-  }, [user?.email]);
-
-  const handleAddFood = (event) => {
+  const handleFoodRequest = (event) => {
     event.preventDefault();
 
     const form = event.target;
 
-    const foodName = form.food_name.value;
-    const foodImage = form.food_image.value;
-    const foodQuantity = form.food_quantity.value;
-    const pickupLocation = form.pickup_location.value;
-    const expiredDate = form.expired_date.value;
-    const foodStatus = form.food_status.value;
+    const userEmail = user?.email;
+    const requestDate = form.req_date.value;
+    const donationMoney = form.donation_money.value;
     const additionalNotes = form.additional_notes.value;
-    const donatorImage = userDb.photo;
-    const donatorName = userDb.name;
-    const donatorEmail = userDb.email;
 
     const food = {
       foodImage,
@@ -38,18 +43,19 @@ const AddFood = () => {
       donatorImage,
       donatorName,
       donatorEmail,
-      foodQuantity,
       pickupLocation,
-      foodStatus,
       expiredDate,
       additionalNotes,
+      userEmail,
+      requestDate,
+      donationMoney
     };
 
     console.log(food);
 
     form.reset();
 
-    fetch("http://localhost:5000/foods", {
+    fetch("http://localhost:5000/foodrequests", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -62,7 +68,7 @@ const AddFood = () => {
         if (data.insertedId) {
           Swal.fire({
             title: "Success!",
-            text: "Successfully Added Food",
+            text: "Successfully Requested Food",
             icon: "success",
             confirmButtonText: "OK",
           });
@@ -72,19 +78,9 @@ const AddFood = () => {
   };
 
   return (
-    <div>
-      <HeroPages name="Add Food"></HeroPages>
-
-      <div className="max-w-5xl mx-auto p-5 md:p-4 lg:p-2 ">
-        <Helmet>
-          <title>Share Eats | Add Food</title>
-        </Helmet>
-
-        <form
-          onSubmit={handleAddFood}
-          className="shadow-lg shadow-[#D70F64] rounded-xl mb-24 lg:px-2 px-5 pt-6 pb-8 hover:shadow-2xl hover:shadow-[#D70F64] mt-10"
-        >
-          {/* Food Info  */}
+    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+      <div className="modal-box">
+        <form onSubmit={handleFoodRequest} className="">
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
             <label
               className="block text-[#D70F64] font-bold mb-2"
@@ -96,8 +92,8 @@ const AddFood = () => {
               className="ring-1 rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight  focus:ring-2 focus:ring-[#D70F64]"
               name="food_name"
               type="text"
-              placeholder="Enter Food Name"
-              required
+              defaultValue={foodName}
+              readOnly
             />
           </div>
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
@@ -111,89 +107,25 @@ const AddFood = () => {
               className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
               name="food_image"
               type="text"
-              placeholder="Enter Food image URL"
-              required
-            />
-          </div>
-          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
-            <label
-              className="block text-[#D70F64] font-bold mb-2"
-              htmlFor="food_quantity"
-            >
-              Food Quantity
-            </label>
-            <input
-              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
-              name="food_quantity"
-              type="number"
-              placeholder="Enter Food Quantity"
-              required
-            />
-          </div>
-          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
-            <label
-              className="block text-[#D70F64] font-bold mb-2"
-              htmlFor="pickup_location"
-            >
-              Pickup Location
-            </label>
-            <input
-              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
-              name="pickup_location"
-              type="text"
-              placeholder="Enter Pickup Location"
-              required
-            />
-          </div>
-          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
-            <label
-              className="block text-[#D70F64] font-bold mb-2"
-              htmlFor="food_status"
-            >
-              Food Status
-            </label>
-            <input
-              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
-              name="food_status"
-              type="text"
-              defaultValue="Available"
-              placeholder="Enter Pickup Location"
+              defaultValue={foodImage}
               readOnly
-              required
             />
           </div>
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
             <label
               className="block text-[#D70F64] font-bold mb-2"
-              htmlFor="expired_date"
+              htmlFor="food_id"
             >
-              Expired Date
+              Food ID
             </label>
             <input
               className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
-              name="expired_date"
+              name="food_id"
               type="text"
-              placeholder="2023-12-10"
-              required
+              defaultValue={_id}
+              readOnly
             />
           </div>
-          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
-            <label
-              className="block text-[#D70F64] font-bold mb-2"
-              htmlFor="additional_notes"
-            >
-              Additional Notes
-            </label>
-            <textarea
-              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
-              name="additional_notes"
-              rows="3"
-              placeholder="Enter Additional Notes"
-              required
-            ></textarea>
-          </div>
-
-          {/* Donator Info  */}
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
             <label
               className="block text-[#D70F64] font-bold mb-2"
@@ -205,7 +137,7 @@ const AddFood = () => {
               className="ring-1 rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight  focus:ring-2 focus:ring-[#D70F64]"
               name="donator_email"
               type="text"
-              defaultValue={userDb.email}
+              defaultValue={donatorEmail}
               readOnly
             />
           </div>
@@ -220,36 +152,122 @@ const AddFood = () => {
               className="ring-1 rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight  focus:ring-2 focus:ring-[#D70F64]"
               name="donator_name"
               type="text"
-              defaultValue={userDb.name}
+              defaultValue={donatorName}
               readOnly
             />
           </div>
           <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
             <label
               className="block text-[#D70F64] font-bold mb-2"
-              htmlFor="donator_image"
+              htmlFor="user_email"
             >
-              Donator Image URL
+              User email
             </label>
             <input
               className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
-              name="donator_image"
+              name="user_email"
               type="text"
-              defaultValue={userDb.photo}
+              defaultValue={user?.email}
               readOnly
             />
+          </div>
+          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
+            <label
+              className="block text-[#D70F64] font-bold mb-2"
+              htmlFor="req_date"
+            >
+              Request Date
+            </label>
+            <input
+              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
+              name="req_date"
+              type="text"
+              defaultValue={formattedDate}
+              readOnly
+            />
+          </div>
+          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
+            <label
+              className="block text-[#D70F64] font-bold mb-2"
+              htmlFor="pickup_location"
+            >
+              Pickup Location
+            </label>
+            <input
+              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
+              name="pickup_location"
+              type="text"
+              defaultValue={pickupLocation}
+              readOnly
+            />
+          </div>
+          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
+            <label
+              className="block text-[#D70F64] font-bold mb-2"
+              htmlFor="expired_date"
+            >
+              Expired Date
+            </label>
+            <input
+              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
+              name="expired_date"
+              type="text"
+              defaultValue={expiredDate}
+              readOnly
+            />
+          </div>
+          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
+            <label
+              className="block text-[#D70F64] font-bold mb-2"
+              htmlFor="donation_money"
+            >
+              Donation Money
+            </label>
+            <input
+              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
+              name="donation_money"
+              type="text"
+              placeholder="Enter Donation Money"
+              required
+            />
+          </div>
+          <div className="mb-4 max-w-screen-xl mx-auto lg:px-10">
+            <label
+              className="block text-[#D70F64] font-bold mb-2"
+              htmlFor="additional_notes"
+            >
+              Additional Notes
+            </label>
+            <textarea
+              className=" rounded w-full py-2 px-3 ring-gray-300 bg-transparent outline-none leading-tight ring-1 focus:ring-2 focus:ring-[#D70F64]"
+              name="additional_notes"
+              rows="3"
+              defaultValue={additionalNotes}
+            ></textarea>
           </div>
           <div className="flex items-center justify-center">
             <input
               type="submit"
-              value="Add Food"
-              className="btn rounded-xl  text-white bg-[#D70F64] hover:bg-transparent hover:border-2 hover:border-[#D70F64] hover:text-[#D70F64] px-10 mt-10"
+              value="Request"
+              className="btn rounded-xl  text-white bg-[#D70F64] hover:bg-transparent hover:border-2 hover:border-[#D70F64] hover:text-[#D70F64] px-10"
             />
           </div>
         </form>
+        <div className="modal-action">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </dialog>
   );
 };
 
-export default AddFood;
+Modal.propTypes = {
+  food: PropTypes.object.isRequired,
+};
+
+export default Modal;
--- src/pages/FoodDetails/FoodDetails.jsx
+++ src/pages/FoodDetails/FoodDetails.jsx
@@ -1,18 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import HeroPages from "../../components/HeroPages/HeroPages";
+import Modal from "../../components/Modal/Modal";
 
 const FoodDetails = () => {
   const food = useLoaderData();
   const {
-    _id,
     foodImage,
     foodName,
-    donatorImage,
     donatorName,
     foodQuantity,
     pickupLocation,
     expiredDate,
-    additionalNotes,
   } = food;
 
   console.log("FoodDetails", food);
@@ -20,16 +18,16 @@ const FoodDetails = () => {
   return (
     <div>
       <HeroPages name="Food Details"></HeroPages>
-      <div className="lg:max-w-7xl mx-auto flex gap-16 flex-col  justify-center items-center my-12 p-5 md:p-4 lg:p-2">
-        <div className="flex flex-col justify-center items-center shadow-xl rounded-xl hover:shadow-2xl p-12">
-          <h2 className="text-2xl mb-2 font-semibold">Donar Information</h2>
+      <div className="max-w-7xl mx-auto flex gap-16 flex-col  justify-center items-center my-12 p-5 md:p-4 lg:p-2">
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl mb-2 font-bold">Donar Information</h2>
           <h3 className="font-semibold">Name: {donatorName}</h3>
           <p>Pickup Location: {pickupLocation}</p>
         </div>
         <div className="shadow-xl rounded-xl hover:shadow-2xl ">
           <img
             src={foodImage}
-            className=" w-full  h-[170px] md:w-[500px] md:h-[300px] rounded-tl-xl rounded-tr-xl"
+            className=" w-[300px]  h-[170px] md:w-[500px] md:h-[300px] rounded-tl-xl rounded-tr-xl"
             alt=""
           />
           <div className="p-4">
@@ -39,7 +37,15 @@ const FoodDetails = () => {
                 Quantity: {foodQuantity}
               </p>
               <p className="text-sm">Expired Date: {expiredDate}</p>
-              <button className="btn bg-[#D70F64] text-white hover:border-2 hover:border-[#D70F64] hover:bg-transparent hover:text-[#D70F64] btn-block">Request</button>
+              <button
+                onClick={() =>
+                  document.getElementById("my_modal_5").showModal()
+                }
+                className="btn bg-[#D70F64] text-white hover:border-2 hover:border-[#D70F64] hover:bg-transparent hover:text-[#D70F64] btn-block"
+              >
+                Request
+              </button>
+              <Modal food={food}></Modal>
             </div>
           </div>
         </div>
